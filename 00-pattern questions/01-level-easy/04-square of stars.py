51bcb4bd6e52c1fc5d22b1377a3cdd0a31c02cb5
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-16 11:21:45 +0530

    alternate answers added

--- 00-pattern questions/01-level-easy/04-square of stars.py
+++ 00-pattern questions/01-level-easy/04-square of stars.py
@@ -3,3 +3,6 @@ for i in range(n):
     for j in range(n):
         print("*", end="")
     print("")
+
+# for i in range(n):
+#     print("*" * n)
--- 00-pattern questions/01-level-easy/07-right angle triangle-stars(inc).py
+++ 00-pattern questions/01-level-easy/07-right angle triangle-stars(inc).py
@@ -3,3 +3,6 @@ for i in range (n + 1):
     for j in range(i):
         print("*", end="")
     print("")
+
+# for i in range(n):
+#     print("*"* (i + 1))
