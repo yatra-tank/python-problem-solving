cc1bdd26f8895548586a8688b6fc66c6045c717b
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-16 12:39:49 +0530

    file added

--- 00-pattern questions/02-level-medium/21-right-aligned-inverted-triangle-stars.py
+++ 00-pattern questions/02-level-medium/21-right-aligned-inverted-triangle-stars.py
@@ -1,5 +1,6 @@
 n = int(input("Enter a number: "))
-for i in range(1, n + 1):
+for i in range(n, 0, -1):
+    print(" " * (n - i), end="")
     for j in range(i):
         print("*", end="")
     print()
