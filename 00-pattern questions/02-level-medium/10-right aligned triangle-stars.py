ff133bc51e94bafbb019735b338f6d1511edddc3
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-16 14:16:57 +0530

    alternate answer added

--- 00-pattern questions/02-level-medium/10-right aligned triangle-stars.py
+++ 00-pattern questions/02-level-medium/10-right aligned triangle-stars.py
@@ -1,3 +1,9 @@
 n = int(input("Enter your number: "))
-for i in range(1, n + 1):
-    print(" " * (n-i) + "*" * i)
+# for i in range(1, n + 1):
+#     print(" " * (n-i) + "*" * i)
+
+for i in range(1, n + 1): 
+    print(" " * (n - i), end="")
+    for j in range(i):
+        print("*", end="")
+    print("")
