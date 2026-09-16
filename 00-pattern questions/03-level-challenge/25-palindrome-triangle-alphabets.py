f6106141fe6cb87d36a34d6ff0741548e1029fbb
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-16 17:35:48 +0530

    challenge question added

---
+++ 00-pattern questions/03-level-challenge/25-palindrome-triangle-alphabets.py
@@ -0,0 +1,7 @@
+n = int(input("Enter a number: "))
+for i in range(1, n + 1):
+    for j in range(1, i + 1):
+        print(chr(64 + j), end=" ")
+    for j in range(i - 1, 0, -1):
+        print(chr(64 + j), end=" ")
+    print()
---
+++ 00-pattern questions/03-level-challenge/30-diamond-pattern-alphabets.py
@@ -0,0 +1,15 @@
+n = int(input("Enter your number: "))
+for i in range(1, n + 1):
+    print("  " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(chr(64 + j), end=" ")
+    for j in range(i - 1, 0, -1):
+        print(chr(64 + j), end=" ")
+    print()
+for i in range(n - 1, 0 , -1):
+    print("  " * (n - i), end="")
+    for j in range(1, i + 1):
+        print(chr(64 + j), end=" ")
+    for j in range(i - 1, 0, -1):
+        print(chr(64 + j), end=" ")
+    print()
