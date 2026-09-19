10b9d5e1212e7c5cc520c6cd45ae5920102ac74b
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-19 15:18:09 +0530

    files added

---
+++ 00-pattern questions/02-level-medium/32-hollow-right-angled-triangle-stars.py
@@ -0,0 +1,8 @@
+n = int(input("Enter your number: "))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if j == 1 or i == n or j == i:
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
---
+++ 00-pattern questions/02-level-medium/34-left-aligned-triangle-stars.py
@@ -0,0 +1,3 @@
+n = int(input("Enter your number: "))
+for i in range(n):
+    print(" " * i + "*" * (i + 1))
