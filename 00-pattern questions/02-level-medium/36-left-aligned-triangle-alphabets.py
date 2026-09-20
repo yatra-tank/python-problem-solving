925969eefb48bff6680114423137c602874f699c
Yatra Tank <yatra.tank.cg@gmail.com>
2026-09-20 23:57:45 +0530

    file added

--- 00-pattern questions/02-level-medium/36-left-aligned-triangle-alphabets.py
+++ 00-pattern questions/02-level-medium/36-left-aligned-triangle-alphabets.py
@@ -1,6 +1,6 @@
 n = int(input("Enter your number: "))
 for i in range(1, n + 1):
-    print(" " * i, end="")
+    print(" "*i, end="")
     for  j in range(1, i + 1):
         print(chr(64 + j), end="")
     print()
