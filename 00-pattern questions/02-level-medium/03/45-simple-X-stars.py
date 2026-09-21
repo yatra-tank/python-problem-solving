bcf18378f23533a6b7708db3cd49a1858b4531bc
yatra-tank <yatra.tank.cg@gmail.com>
2026-09-21 22:50:27 +0530

    files added

---
+++ 00-pattern questions/02-level-medium/03/45-simple-X-stars.py
@@ -0,0 +1,8 @@
+n = int(input("Enter the number: "))
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        if i == j or (i + j) == (n + 1):
+            print("*", end="")
+        else:
+            print(" ", end="")
+    print()
